0f676ba1762709410bf333ea75245ea9a72b8bd3
Fraser Beaton <fraser0319@gmail.com>
2019-01-06 14:59:54 +0000

    updated the footer with github link and email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,7 @@ class Footer extends React.Component {
       <section className="section" id="footer">
         <footer class="footer">
           <div class="content has-text-centered">
-            <p>
-              <strong>Bulma</strong> by{' '}
-              <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-              licensed
-              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
-              The website content is licensed{' '}
-              <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-                CC BY NC SA 4.0
-              </a>
-              .
-            </p>
+            <p>Build and maintained by <a href="https://github.com/Fraser0319">Fraser Beaton</a></p>
           </div>
         </footer>
       </section>
